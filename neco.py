7a955423ae17188d7f4079921c2d5ec04c7b93a6
kaja-austova <144602439+kaja-austova@users.noreply.github.com>
2023-11-01 19:55:36 +0000

    .

--- neco.py
+++ neco.py
@@ -1,24 +1,28 @@
 # spolupráce s Juli Svatošovou a Mariánkou
-a = 5
-b = 10
-c = 8
+
+a = 0
+b = 6
+c = 0
+
+def solve_quadratic_equation():
+    print (a,"x**2 +",b,"x +",c,"=0") #z nějakého důvodu se nevypisuje nic, ale ani to nehází chyby, nevím proč
 
 D = b * b - 4 * a * c
 if a == 0:
     if b==0:
-      if c==0:
-        print ("x náleží do reálných čísel")
-      else:
-        print("Oops")
+        if c==0:
+            print ("x náleží do reálných čísel")
+        else:
+            print("Oops")
     else:
-      print ("1",-c/b)   
+        print ("1",-c/b)   
 else:
-   if D == 0:
-    print("1:", -b / (2 * a))
-   elif D > 0:
-    print(
+    if D == 0:
+        print("1:", -b / (2 * a))
+    elif D > 0:
+        print (
         "1: ", (-b + D ** 0.5) / (2 * a),
         "2: ", (-b - D ** 0.5) / (2 * a)
        )
-   else:
-    print("Oops")
+    else:
+        print("Oops")
