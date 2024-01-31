369249bc256e731df0d5a1cf2e26e62845cbadf6
kaja-austova <144602439+kaja-austova@users.noreply.github.com>
2024-01-31 10:40:56 +0000

    Potvrdit

--- web.py
+++ web.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def pondeli_def():
+    return render_template("neco.html", title="Jídelníček", den = "Jídelníček")
+
 @app.route('/pondeli')
 def pondeli_def():
     return render_template("web.html", title="Pondělí", den = "Pondělí", text = "Přejeme Vám hezký začátek týdne.", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/pubhtml/sheet?headers=false&amp;gid=0&amp;range=A6:G10")
