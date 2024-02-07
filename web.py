3d31b862441330a51addbdff42b2445ab16312cc
kaja-austova <144602439+kaja-austova@users.noreply.github.com>
2024-02-07 10:19:02 +0000

    ..

--- web.py
+++ web.py
@@ -8,20 +8,21 @@ def index_def():
 
 @app.route('/pondeli')
 def pondeli_def():
-    return render_template("web.html", title="Pondělí", den = "Pondělí", text = "Přejeme Vám hezký začátek týdne.", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/pubhtml/sheet?headers=false&amp;gid=0&amp;range=A6:G10")
+    # return render_template("web.html", title="Pondělí", den = "Pondělí", text = "Přejeme Vám hezký začátek týdne.", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/edit#gid=0")
+    return render_template("web.html", title="Pondělí", den = "Pondělí", text = "Přejeme Vám hezký začátek týdne.", bunka_start="A6", bunka_konec="G10")
 
 @app.route('/utery')
 def utery_def():
-    return render_template("web.html", title="Úterý", den = "Úterý", text = "To nejhorší máte za sebou.", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/pubhtml/sheet?headers=false&amp;gid=0&amp;range=A6:G10")
+    return render_template("web.html", title="Úterý", den = "Úterý", text = "To nejhorší máte za sebou.", bunka_start="A12", bunka_konec="G17")
 
 @app.route('/streda')
 def streda_def():
-    return render_template("web.html", title="Středa", den = "Středa", text = "Už jste v polovině!", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/pubhtml/sheet?headers=false&amp;gid=0&amp;range=A6:G10")
+    return render_template("web.html", title="Středa", den = "Středa", text = "Už jste v polovině!", bunka_start="A20", bunka_konec="G24" )
 
 @app.route('/ctvrtek')
 def čtvrtek_def():
-    return render_template("web.html", title="Čtvrtek", den = "Čtvrtek", text = "Před Vámi je cílová rovinka.", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/pubhtml/sheet?headers=false&amp;gid=0&amp;range=A6:G10")
+    return render_template("web.html", title="Čtvrtek", den = "Čtvrtek", text = "Před Vámi je cílová rovinka.", bunka_start="A27", bunka_konec="G31")
 
 @app.route('/patek')
 def patek_def():
-    return render_template("web.html", title="Pátek", den = "Pátek", text = "Ten všemi očekávaný den je tu.", tabulka = "https://docs.google.com/spreadsheets/d/1JpEUpUJ3slFP1y2PgJV1J_2_sBf5VOek4TUcq90P_Cs/pubhtml/sheet?headers=false&amp;gid=0&amp;range=A6:G10")
+    return render_template("web.html", title="Pátek", den = "Pátek", text = "Ten všemi očekávaný den je tu.", bunka_start="A34", bunka_konec="G38" )
